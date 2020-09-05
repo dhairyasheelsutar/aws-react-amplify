37c1764e2d8a0786d39167da5ed38b69e7106bca
Piyush Bendale <piyush@thetechnolover.com>
2020-09-05 22:50:19 +0530

    ui done for friends and invite

--- src/App.js
+++ src/App.js
@@ -1,29 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
 
-  const userId = window.location.href.split("?")[1].split("=")[1];
+  constructor() {
+    super();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      senderId: '',
+      friendId: '',
+      showData: false
+    }
+  }
+
+
+  handleChangeUser = (event) => {
+    this.setState({senderId: event.target.value});
+  }
+
+  handleChangeFriendId = (event) => {
+    this.setState({friendId: event.target.value});
+  }
+
+  fetchData = () => {
+    
+    this.setState({
+      showData: true
+    });
+  }
+
+  sendFriendReq = () => {
+    console.log(this.state);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        
+        <div id="selectUser">
+          <h4>Select User</h4>
+          <input type="text" onChange={this.handleChangeUser} /> 
+          <button onClick={this.fetchData}>Submit</button>
+        </div>
+        <br />
+        <br />
+        <br />
+  
+        { 
+          
+          this.state.showData ? 
+          
+          <div>
+            <div id="selectUserToSendReq">
+            <h4>Select Friend want to add as friend</h4>
+            <input type="text" onChange={this.handleChangeFriendId} /> 
+            <button onClick={this.sendFriendReq}>Submit</button>
+          </div>
+    
+          
+          <br />
+          <br />
+          <br />
+    
+    
+          <div id="seePendingInv">
+            <h4>Invites</h4>
+            <p>mewtwo.X  -----------------------------   <button>Add</button> <button>Cancel</button></p>
+            
+          </div>
+    
+          
+          <br />
+          <br />
+          <br />
+    
+          <div id="friends">
+            <h4>Friends</h4>
+            <p>mewtwo.X  -----------------------------   <button>Remove</button></p>
+            
+          </div>
+    
+    
+          <br />
+          <br />
+          <br />
+    
+          <div id="sentRequests">
+            <h4>Sent Requests</h4>
+            <p>mewtwo.X  -----------------------------   <button>Cancel</button></p>
+            
+          </div> 
+          </div> 
+          
+          :
+
+          <div></div>
+        }
+  
+      </div>
+    );
+  }
 }
 
 export default App;
